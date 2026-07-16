a1148f09f8bd8d08d8088790e5ff8cf0a7ea77de
hameedsharaf07-pixel <hameedsharaf1234@gmail.com>
2026-07-16 14:40:14 +0530

    Create DAY 2-1528 Shuffle String.java

---
+++ DAY 2-1528 Shuffle String.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        int n = s.length();
+        char[] shuffled = new char[n];
+        
+        for (int i = 0; i < n; i++) {
+            shuffled[indices[i]] = s.charAt(i);
+        }  
+        return new String(shuffled);
+    }
+}
