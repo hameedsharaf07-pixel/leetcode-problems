1af73c06d60b0f7f82c615af121c4f8c4430e3fe
hameedsharaf07-pixel <hameedsharaf1234@gmail.com>
2026-07-27 13:40:47 +0530

    383-Ransom Note.java

---
+++ 383-Ransom Note.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if (ransomNote.length() > magazine.length()) {
+            return false;
+        }
+        int[] letterCounts = new int[26];
+
+        for (char c : magazine.toCharArray()) {
+            letterCounts[c - 'a']++;
+        }
+        for (char c : ransomNote.toCharArray()) {
+            if (letterCounts[c - 'a'] == 0) {
+                return false; 
+            }
+            letterCounts[c - 'a']--;
+        }
+        return true;
+    }
+}
